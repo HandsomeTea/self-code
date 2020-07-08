8e16518d759387cc8d6754ab858a80a529ec12b1
haifeng_liu@bizconf.cn <haifeng_liu@bizconf.cn>
2020-07-08 20:24:51 +0800

    code update

--- js/keyword-search.js
+++ js/keyword-search.js
@@ -1,18 +1,38 @@
-/**vue项目中饿关键字搜索
+/**vue项目中列表数据的一般代码
  * 待输入完成再搜索
  */
+const data = () => {
+    return {
+        isAlready: null,
+        keyword: '',
+        waitForKeywordInputEndTimer: null,
+        paging: {
+            page: 1,
+            limit: 15,
+            total: 0
+        },
+        list: [],
+        multipleChosed: []
+    }
+},
 const watch = {
     keyword() {
         const waitTime = 600;
         if (this.waitForKeywordInputEndTimer === null) {
             this.waitForKeywordInputEndTimer = setTimeout(() => {
-                this.getUserListAndSetListChosedData();
+                this.getList();
             }, waitTime);
         } else {
             clearTimeout(this.waitForKeywordInputEndTimer);
             this.waitForKeywordInputEndTimer = setTimeout(() => {
-                this.getUserListAndSetListChosedData();
+                this.getList();
             }, waitTime);
         }
-    }
+    },
+    'paging.page'() {
+        this.getList();
+    },
+    'paging.limit'() {
+        this.getList();
+    },
 }
